463b2300b538707ec5c1de47f229e89cdb6a7c43
Peter Steinberg <bklynpeter@gmail.com>
2022-06-07 15:40:39 -0400

    tweak

---
+++ tests/App.test.js
@@ -0,0 +1,7 @@
+import App from "../client/App";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+test("full app rendering/navigating", async () => {
+  expect(true).toEqual(true);
+});
