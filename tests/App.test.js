b9848bfa2a3403fc4a87a93bc87b151d762d2127
Peter Steinberg <bklynpeter@gmail.com>
2022-06-07 15:55:49 -0400

    tweak

---
+++ tests/App.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from "../client/Components/Login";
+import SignUp from "../client/Components/Signup";
+import App from "../client/App";
